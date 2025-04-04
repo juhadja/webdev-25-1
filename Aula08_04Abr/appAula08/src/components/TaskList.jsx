e263ab6174e1875096c036b9f85508d759fe796e
Julia Hadja <juliahadja@gmail.com>
2025-04-04 11:42:53 -0300

    lista-tarefas-ok

--- Aula08_04Abr/appAula08/src/components/TaskList.jsx
+++ Aula08_04Abr/appAula08/src/components/TaskList.jsx
@@ -1,3 +1,4 @@
+import {Icon} from "@iconify/react/dist/iconify.js"
 import AddTaskBox from "./AddTaskBox";
 import TaskListItem from "./TaskListItem"
 import { useState } from "react"
@@ -10,6 +11,12 @@ export default function TaskList() {
         setTasks([...tasks, taskName])
     }
 
+    function onClickRemove(idx){
+        console.log("onClickRemove",idx)
+        tasks.splice(idx,1)
+        setTasks([...tasks])
+    }
+
     return (
         <>
             <AddTaskBox onAddTaskName={addTask}/>
@@ -18,8 +25,16 @@ export default function TaskList() {
                     Lista de tarefas: 
                 </div>
                 <hr className="my-2" />
+                {tasks.length == 0 &&
+                    <div className="bg-gray-300 p-1 text-lg rounded">
+                        <div className="flex items-center gap-2">
+                            <icon icon='mdi:info' className="text-4x1 text-red-600" />
+                           Nenhuma tarefa foi adicionada !
+                        </div>
+                    </div>
+                }
                 <div>
-                    { tasks.map((elem,idx) => <TaskListItem key={idx} taskName={elem} />) }
+                    { tasks.map((elem,idx) => <TaskListItem key={idx} onRemove={onClickRemove} idx={idx} taskName={elem} ultimo={idx == tasks.length-1} />) }
                 </div>
             </div>
         </>
--- Aula08_04Abr/appAula08/src/components/TaskListItem.jsx
+++ Aula08_04Abr/appAula08/src/components/TaskListItem.jsx
@@ -1,21 +1,62 @@
 import { Icon } from "@iconify/react/dist/iconify.js"
+import { useState } from "react"
 
-export default function TaskListItem({ taskName }) {
+export default function TaskListItem({ taskName, idx, onRemove, ultimo }) {
 
+    const[ done, setDone ] = useState([true])
+
+    function clickRemove(){
+        console.log("remover item",idx)
+        onRemove(idx)
+    }
+    function doneClick(){
+        console.log("done")
+        setDone(!done)
+
+    }
 
     return (
         <>
             <div className="bg-gray-200 p-2 rounded my-1 flex items-center justify-between">
-                <div>
-                    { taskName }
+                <div className="flex items-center gap-4">
+                    <div>
+                        {idx > 0 &&
+                            <div>
+                                <Icon icon="mdi:triangle" />
+                            </div>
+                        }
+                        {!ultimo &&
+                            <div>
+                                <Icon icon="mdi:triangle-down" />
+                            </div>
+                        }
+                        
+                    </div>
+                    <div>
+                        { taskName }
+                    </div>
                 </div>
                 <div className="flex items-center gap-2">
-                    <div className="border border-black rounded-2xl px-1.5 pt-1 bg-gray-800 text-white">
+                <div onClick={clickRemove} className="border  rounded-2xl px-1.5 pt-1 bg-gray-800 text-white">
+                        <Icon icon="mdi:edit" />
+                    </div>
+                    <div onClick={clickRemove} className="border  rounded-2xl px-1.5 pt-1 bg-red-800 text-white">
                         <Icon icon="mdi:delete" />
                     </div>
-                    <div className="border border-black rounded-2xl px-1.5 pt-1 bg-green-800 text-white">
-                        <Icon icon="mdi:check" />
-                    </div>
+
+                    {
+                        done &&
+                        <div onClick={doneClick} className="border rounded-2xl px-1.5 pt-1 bg-green-800 text-white">
+                            <Icon icon="mdi:check" />
+                        </div>
+                    }
+                    {
+                        !done &&
+                        <div onClick={doneClick} className="border rounded-2xl px-1.5 pt-1 text-black">
+                            <Icon icon="mdi:check" />
+                        </div>
+                    }
+                    
                 </div>
             </div>
         </>
